b3a418a2d02728c75f3be1cba40600decd59b290
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2017-01-24 15:46:09 -0200

    0.1

--- www/js/index.js
+++ www/js/index.js
@@ -20,18 +20,16 @@ function pronto(){
             var dados = dadosJSON.listaFotos;
             var listaFotos = "<ul class='table-view'>";
             for(i=0; i<dados.length; i++ ) {
-                listaFotos += '<li class="table-view-cell"><a class="navigate-right" onclick="novaPaginaFotos('+ dados[i].listaNome +')">' + dados[i].listaNome + "</a></li>"; }
+                listaFotos += '<li class="table-view-cell"><a class="navigate-right" onclick="novaPaginaFotos('+ i +')">' + dados[i].listaNome + "</a></li>"; }
             listaFotos += "</ul>";
             document.getElementById("conteudo").innerHTML = listaFotos;
         }
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/fotos.html') {
-            var dados = dadosJSON.listaFotos;
+            var dados = dadosJSON.listaFotos[paginaAtual];
             var fotos = '<div class="slider" id="mySlider"><div class="slide-group">';
-
-
-
-
-
+            var fotos += '<div class="slide"><img src="' + dados.fotos[0] + '"><span class="slide-text"><span class="icon icon-left-nav"></span> Escorregue para a esquerda </span></div>';
+            for(i=1; i<dados.fotos.length; i++ ) {
+                var fotos += '<div class="slide"><img src="' + dados.fotos[i] + '"></div>'; }
             var fotos += '</div></div>';
             document.getElementById("conteudo").innerHTML = fotos;
         }
