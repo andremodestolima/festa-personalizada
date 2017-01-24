1ad33676b10766a9eb68c6aa4608dfae0a48d84a
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2017-01-24 13:10:07 -0200

    0.1

--- www/js/index.js
+++ www/js/index.js
@@ -8,13 +8,34 @@ function pronto(){
         dadosJSON = data;
     });
 
+    function novaPaginaFotos(qual){
+        paginaAtual = qual;
+        window.PUSH({url: 'fotos.html', transition: 'slide-in'});
+    }
+
     function ratchetPronto() {
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/index.html') {
         }
+        if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/listaFotos.html') {
+            var dados = dadosJSON.listaFotos;
+            var listaFotos = "<ul class='table-view'>";
+            for(i=0; i<dados.length; i++ ) {
+                listaFotos += '<li class="table-view-cell"><a class="navigate-right" onclick="novaPaginaFotos('+ dados[i].listaNome +')">' + dados[i].listaNome + "</a></li>"; }
+            listaFotos += "</ul>";
+            document.getElementById("conteudo").innerHTML = listaFotos;
+        }
+        if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/fotos.html') {
+            var dados = dadosJSON.listaFotos;
+            var fotos = '<div class="slider" id="mySlider"><div class="slide-group">';
+
+
+
+
+
+            var fotos += '</div></div>';
+            document.getElementById("conteudo").innerHTML = fotos;
+        }
     }
-
-
-
 }
 
 document.addEventListener('deviceready', pronto, false);
