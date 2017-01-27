048f6742bceb5786e09f244f9ba6c8d988c61110
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2017-01-27 14:31:08 -0200

    0.1

--- www/js/index.js
+++ www/js/index.js
@@ -2,39 +2,37 @@ function pronto(){
     window.addEventListener('push', ratchetPronto);
     window.PUSH = PUSH;
     var paginaAtual = "";
-    var dadosJSON;
+    var listaFotos;
+    var tamanhoLista = 0;
+    var fotos=[];
 
     jQuery.getJSON("dados.json", function(data){
-        dadosJSON = data;
+        listaFotos = "<ul class='table-view'>";
+        for(i=0; i<data.listaFotos.length; i++ ) {
+            tamanhoLista += 1;
+            listaFotos += '<li class="table-view-cell"><a class="navigate-right" id="'+ i +'">' + data.listaFotos[i].listaNome + "</a></li>";
+            fotos[i] = '<div class="slider" id="mySlider"><div class="slide-group">';
+            fotos[i] += '<div class="slide"><img src="' + data.listaFotos[i].fotos[0] + '"><span class="slide-text"><span class="icon icon-left-nav"></span> Escorregue para a esquerda </span></div>';
+            for (b = 1; b < data.listaFotos[i].fotos.length; b++) {
+                fotos[i] += '<div class="slide"><img src="' + data.listaFotos[i].fotos[b] + '"></div>'; }
+            fotos[i] += '</div></div>';
+        }
+        listaFotos += "</ul>";
     });
-
-    var listaFotos = "<ul class='table-view'>";
-    for(i=0; i<dadosJSON.listaFotos.length; i++ ) {
-        listaFotos += '<li class="table-view-cell"><a class="navigate-right" id="'+ i +'">' + dadosJSON.listaFotos[i].listaNome + "</a></li>"; }
-    listaFotos += "</ul>";
-
-    function novaPaginaFotos(qual){
-        paginaAtual = qual;
-        window.PUSH({url: 'fotos.html', transition: 'slide-in'});
-    }
 
     function ratchetPronto() {
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/listaFotos.html') {
             navigator.vibrate(200);
             document.getElementById("conteudo").innerHTML = listaFotos;
-            for(i=0; i<dados.length; i++ ) {
-                document.getElementById(String(i)).addEventListener("click", function(){novaPaginaFotos(i)}, false); }
+            for(i=0; i<tamanhoLista.length; i++ ) {
+                document.getElementById(String(i)).addEventListener("click", function(){ paginaAtual = i; window.PUSH({url: 'fotos.html', transition: 'slide-in'});}, false); }
         }
         if (document.location.href.substring(document.location.href.lastIndexOf('/')) == '/fotos.html') {
+            alert(paginaAtual);
+            alert(tamanhoLista);
             navigator.vibrate(200);
-            var dados = dadosJSON.listaFotos[paginaAtual];
-            var fotos = '<div class="slider" id="mySlider"><div class="slide-group">';
-            fotos += '<div class="slide"><img src="' + dados.fotos[0] + '"><span class="slide-text"><span class="icon icon-left-nav"></span> Escorregue para a esquerda </span></div>';
-            for(i=1; i<dados.fotos.length; i++ ) {
-                fotos += '<div class="slide"><img src="' + dados.fotos[i] + '"></div>'; }
-            fotos += '</div></div>';
-            document.getElementById("conteudo").innerHTML = fotos;
-            document.getElementById("tituloFotos").innerHTML = dados.listaNome;
+            document.getElementById("conteudo").innerHTML = fotos[paginaAtual];
+            document.getElementById("tituloFotos").innerHTML = "FOTOS";
         }
     }
 }
