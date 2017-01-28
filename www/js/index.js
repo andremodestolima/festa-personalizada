0596fea095efdabf1d49e65494aa432939234270
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2017-01-28 14:54:42 -0200

    0.1

--- www/js/index.js
+++ www/js/index.js
@@ -12,7 +12,7 @@ function pronto(){
             tamanhoLista += 1;
             listaFotos += '<li class="table-view-cell"><a class="navigate-right" id="'+ i +'">' + data.listaFotos[i].listaNome + "</a></li>";
             fotos[i] = '<div class="slider" id="mySlider"><div class="slide-group">';
-            fotos[i] += '<div class="slide"><img src="' + data.listaFotos[i].fotos[0] + '"><span class="slide-text"><span class="icon icon-left-nav"></span> Escorregue para a esquerda </span></div>';
+            fotos[i] += '<div class="slide"><img src="' + data.listaFotos[i].fotos[0] + '" class="foto"><span class="slide-text"><span class="icon icon-left-nav"></span> Escorregue para a esquerda </span></div>';
             for (b = 1; b < data.listaFotos[i].fotos.length; b++) {
                 fotos[i] += '<div class="slide"><img src="' + data.listaFotos[i].fotos[b] + '"></div>'; }
             fotos[i] += '</div></div>';
